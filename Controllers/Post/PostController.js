6ba4c003ccff221349c96e2e30dd7633d8b411a3
Ashwiner <techmindashwin78@gmail.com>
2024-12-25 21:40:21 +0530

    created user schema

---
+++ Controllers/Post/PostController.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+
+const PostController = {
+    /**
+     * Method to create post
+     */
+    async createPost() {
+
+    },
+
+     /**
+     * Method to create post
+     */
+     async updatePost() {
+
+     },
+
+    /**
+     * Method to list all post of user
+     */
+    async getAllPostOfUser() {
+
+    },
+
+    /**
+     * Method to delete the post
+     */
+    async deletePost() {
+
+    }
+}
